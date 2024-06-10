4697c9be6911f9f29b375b5a7fb48cf5bb0b159e
Dipesh Chaudhary <149223446+Dipesh0310@users.noreply.github.com>
2024-06-09 22:54:22 -0400

    #4 Creating Category screen

--- screens/CategoryScreen.js
+++ screens/CategoryScreen.js
@@ -7,59 +7,5 @@ export default function CategoryScreen() {
   const [categories, setCategories] = useState([]);
   const navigation = useNavigation();
 
-  useEffect(() => {
-    async function fetchDataFromFirebase() {
-      try {
-        const categoriesFromFirebase = await fetchCategories();
-        console.log('Fetched categories:', categoriesFromFirebase);
-        setCategories(categoriesFromFirebase);
-      } catch (error) {
-        console.error('Error fetching categories:', error);
-      }
-    }
-
-    fetchDataFromFirebase();
-  }, []);
-
-  const handleCategoryPress = (categoryId) => {
-    navigation.navigate('SubCategoryScreen', { categoryId });
-  };
-
-  return (
-    <View style={styles.container}>
-      <FlatList
-        data={categories}
-        keyExtractor={(item) => item.id}
-        renderItem={({ item }) => (
-          <TouchableOpacity style={styles.item} onPress={() => handleCategoryPress(item.id)}>
-            <Text style={styles.itemText}>{item.name}</Text>
-          </TouchableOpacity>
-        )}
-      />
-    </View>
-  );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    alignItems: 'center',
-    justifyContent: 'center',
-    padding: 20,
-    backgroundColor: '#f5f5f5',
-  },
-  item: {
-    width: '100%',
-    padding: 15,
-    borderWidth: 1,
-    borderColor: '#ccc',
-    borderRadius: 10,
-    marginBottom: 10,
-    backgroundColor: '#fff',
-  },
-  itemText: {
-
-    fontSize: 18,
-    fontWeight: 'bold',
-  },
-});
