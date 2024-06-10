222a9b013dfb0a4c9648147d3cc243a4d335c55e
Anjila Gurung <147683233+Angeelaa@users.noreply.github.com>
2024-06-09 22:15:32 -0400

    #5 Navigations created and starting Home screen creation

--- App.js
+++ App.js
@@ -2,10 +2,13 @@ import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import Icon from 'react-native-vector-icons/FontAwesome';
 import SignInScreen from './screens/SignInScreen';
 import SignUpScreen from './screens/SignUpScreen';
 import HomeScreen from './screens/HomeScreen';
 import CartScreen from './screens/CartScreen';
+import HelpSupportScreen from './screens/HelpSupportScreen';
+import AboutScreen from './screens/AboutScreen';
 import SettingsScreen from './screens/SettingsScreen';
 import WishListScreen from './screens/WishListScreen';
 import SubCategoryScreen from './screens/SubCategoryScreen';
@@ -19,6 +22,25 @@ const Tab = createBottomTabNavigator();
 function MainTabNavigator() {
   return (
     <Tab.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ color, size }) => {
+          let iconName;
+          if (route.name === 'Home') {
+            iconName = 'home';
+          } else if (route.name === 'CategoryTab') {
+            iconName = 'th-list';
+          } else if (route.name === 'WishListTab') {
+            iconName = 'heart';
+          } else if (route.name === 'CartTab') {
+            iconName = 'shopping-cart';
+          } else if (route.name === 'SettingsTab') {
+            iconName = 'cog';
+          }
+          return <Icon name={iconName} size={size} color={color} />;
+        },
+        tabBarActiveTintColor: 'tomato',
+        tabBarInactiveTintColor: 'gray',
+      })}
     >
       <Tab.Screen name="Home" component={HomeScreen} options={{ headerShown: false }} />
       <Tab.Screen name="CategoryTab" component={CategoryScreen} options={{ title: 'Categories' }} />
@@ -36,6 +58,10 @@ export default function App() {
         <Stack.Screen name="SignIn" component={SignInScreen} options={{ title: 'Sign In', headerShown: false }} />
         <Stack.Screen name="SignUp" component={SignUpScreen} options={{ title: 'Sign Up', headerShown: false }} />
         <Stack.Screen name="Main" component={MainTabNavigator} options={{ headerShown: false }} />
+        <Stack.Screen name="SubCategoryScreen" component={SubCategoryScreen} options={{ title: 'Subcategories' }} />
+        <Stack.Screen name="ProductScreen" component={ProductScreen} options={{ title: 'Products' }} />
+        <Stack.Screen name="HelpSupport" component={HelpSupportScreen} />
+        <Stack.Screen name="About" component={AboutScreen} />
       </Stack.Navigator>
     </NavigationContainer>
   );
